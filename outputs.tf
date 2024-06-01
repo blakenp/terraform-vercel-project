9299360b9554b7803cec47aef9c41e63efeae0dc
blakenp <blake.ptrsn18@gmail.com>
2024-05-31 22:43:22 -0600

    add output for vercel deployment by id of the deployment

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "vercel_project_id" {
 output "vercel_project_url" {
   value = vercel_project_domain.config.domain
 }
+
+output "vercel_deployment_id" {
+  value = vercel_deployment.config.id
+}
